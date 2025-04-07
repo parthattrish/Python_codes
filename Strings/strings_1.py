fc54e0d787f316c5cd0ad640fb296aea25a85b7c
Parth Sharma <parthattrish@gmail.com>
2025-04-07 20:04:24 +0530

    strings_1.py

--- Strings/strings_1.py
+++ Strings/strings_1.py
@@ -1,1 +1,10 @@
-
+a=input("enter your name :")
+print("this is a string :",a)
+b= "kabootar"#initialisation of a string
+print(b[0:3])#string slicing by positive indices
+print(b[-9:-5])#string slices by negative slicing
+'''string can be accepted or input or used in python with single double or triple quotes
+triple quotes are used for multiline comments also'''
+print('aaloo ki sabzi')
+print("aa gaye h meerut ke mashoor nankhatayi waale")
+print('''jai hind''')
