2419f90264b5a6ed5583e25dc1f52dd8e3480af7
Parth Sharma <parthattrish@gmail.com>
2025-04-07 20:19:45 +0530

    slicing.py

---
+++ Strings/slicing.py
@@ -0,0 +1,10 @@
+#string slicing methods :-
+a=input("enter a 5 word string :")
+print("slicing your string from word 1 to 4")
+b=a[0:4]#positive slicing 
+print(b)
+print(a[-5:-1])#negative slicing
+#string slicing with skip values
+c="aaloopoori"
+print("every third index from indices 0 to 8 :")
+print(c[0:9:3])
