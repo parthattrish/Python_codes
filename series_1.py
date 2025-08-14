7839a05f0bc1a03fb8a2d651b4f81bd1654df5ed
Parth Sharma <parthattrish@gmail.com>
2025-08-14 17:20:46 +0530

    Delete series_1.py

--- series_1.py
+++
@@ -1,8 +0,0 @@
-#series are the 1 d data structures in python.
-import pandas as pd
-x=[1,2,3,4,5]
-a=pd.Series(x)
-print(a)
-b=pd.Series(x,index=["a","s","d","f","e"],dtype="float",name="numbers")
-print(b)
-print(a[2],b[0])
